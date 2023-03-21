2e74247aa7fe7cf63fc7a2638d1f41fa0f329f06
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-21 15:44:25 +0100

    API collegata

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            emailList: []
+        }
+    },
+    beforeMount() {
+        for (let i = 1; 1 <= 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data.response);
+            })
         }
     }
 }).mount('#app');
+
