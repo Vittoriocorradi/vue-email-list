1fdb81959b019627849aa76d009ffcf072a46514
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-21 16:39:02 +0100

    Stampati insieme con setTimeout (sbagliato)

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
 const { createApp } = Vue;
 
+const app = createApp({
+    
+})
+
 createApp({
     data() {
         return {
-            emailList: []               //Lista email
+            emailList: [],               //Lista email
+            nameList: ['Piero',
+        'Paolo','Poldo']
         }
     },
     beforeMount() {
         // Ciclo per avere dieci email
+        const fragment = [];
         for (let i = 1; i <= 10; i++) {
         // Pusha email generata con API dentro emailList
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                this.emailList.push(response.data.response);
+                fragment.push(response.data.response);
             })
         }
+        // Montati insieme con timeout
+        setTimeout(() => {
+            this.emailList = [...fragment];
+        }, 500);
+
     }
 }).mount('#app');
 
